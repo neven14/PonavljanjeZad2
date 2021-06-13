c740706e705a70e63f40c63d9290591ec1f3f8e4
Neven <58472164+neven14@users.noreply.github.com>
2021-06-13 09:29:10 +0200

    Add files via upload

---
+++ PonavljanjeZad2/PonavljanjeZad2/KlasaD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonavljanjeZad2
+{
+    class KlasaD
+    {
+		public bool Suprotno(bool Broj)
+		{
+			return !Broj;
+		}
+		public int Suprotno(int Broj)
+		{
+			return Broj * (-1);
+		}
+	}
+}
---
+++ PonavljanjeZad2/PonavljanjeZad2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonavljanjeZad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Broj;
+            KlasaD MainObjekt = new KlasaD();
+            bool Uvjet = MainObjekt.Suprotno(true);
+            Broj = MainObjekt.Suprotno(50);
+            Console.WriteLine("Suprotno od true je =  " + Uvjet);
+            Console.WriteLine("Suprotno od 50 je =  " + Broj);
+            Console.ReadKey();
+        }
+    }
+}
